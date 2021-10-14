1ffa25bf8d4c52bd8945d45897f0ac10a1d6903a
a-omori-yumemi <a_omori@yumemi.co.jp>
2021-10-14 11:27:50 +0900

    add docker

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/a-omori-yumemi/YumetterAPI
+
+go 1.17
+
+require (
+	github.com/jmoiron/sqlx v1.3.4 // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.9 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.0.1 // indirect
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/labstack/gommon/log"
+)
+
+type DBConfig struct {
+	Port     string
+	Host     string
+	Password string
+	Database string
+	User     string
+}
 
 func main() {
+	conf := DBConfig{
+		Port:     os.Getenv("DB_PORT"),
+		Host:     os.Getenv("DB_HOST"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		Database: os.Getenv("DB_DATABSE"),
+	}
+	dsn := conf.User + ":" + conf.Password + "@tcp(" + conf.Host + ":" + conf.Port + ")/" + conf.Database + "?parseTime=true&multiStatements=true"
+	fmt.Print(dsn)
+	db, err := sqlx.Open("mysql", dsn)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS TEST (id INT PRIMARY KEY, text VARCHAR(100))")
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	id := time.Now().UnixMicro() % 10000000
+	_, err = db.Exec("INSERT INTO TEST (id, text) values (?, 'help!')", id)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	rows := []struct {
+		Id   int    `db:"id"`
+		Text string `db:"text"`
+	}{}
+	err = db.Select(&rows, "SELECT * FROM TEST")
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	for _, row := range rows {
+		log.Print(row.Id, row.Text)
+	}
+	time.Sleep(10)
 	fmt.Println("Hello Docker")
 }
