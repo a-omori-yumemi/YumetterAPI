41404467c52ab0ebac8778379067a74cf10668dd
a-omori-yumemi <a_omori@yumemi.co.jp>
2021-10-14 16:23:27 +0900

    impl auth usacase

---
+++ usecase/JWTAuth.go
@@ -0,0 +1,65 @@
+package usecase
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/a-omori-yumemi/YumetterAPI/model"
+	"github.com/a-omori-yumemi/YumetterAPI/repository"
+	"github.com/golang-jwt/jwt"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type JWTAuthenticator struct {
+	IAuthenticator
+	userRepo  repository.IUserRepository
+	secretKey string
+}
+
+type Claim struct {
+	jwt.StandardClaims
+	UsrID model.UsrIDType
+}
+
+func NewJWTAuthenticator(userRepo repository.IUserRepository, secretKey string) *JWTAuthenticator {
+	return &JWTAuthenticator{userRepo: userRepo, secretKey: secretKey}
+}
+
+func (a *JWTAuthenticator) Login(name model.UserName, password model.Password) (string, error) {
+	user, err := a.userRepo.FindUserByName(name)
+	if err != nil {
+		return "", err
+	}
+	err = bcrypt.CompareHashAndPassword(user.HashedPassword, []byte(password))
+	if err != nil {
+		return "", fmt.Errorf("failed to login")
+	}
+
+	token := jwt.New(jwt.SigningMethodHS256)
+	expire := time.Now().Add(time.Hour * 24 * 3)
+	token.Claims = &Claim{
+		jwt.StandardClaims{
+			ExpiresAt: expire.Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.UsrID,
+	}
+	return token.SignedString([]byte(a.secretKey))
+}
+
+func (a *JWTAuthenticator) UseSession(tokenString string) (model.UsrIDType, error) {
+	// Parse the token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// since we only use the one private key to sign the tokens,
+		// we also only use its public counter part to verify
+		return []byte(a.secretKey), nil
+	})
+	if err != nil {
+		return 0, fmt.Errorf("invalid session")
+	}
+	if claim, ok := token.Claims.(*Claim); ok {
+		return claim.UsrID, err
+	} else {
+		return 0, fmt.Errorf("invalid session")
+	}
+}
---
+++ usecase/usecase.go
@@ -0,0 +1,5 @@
+package usecase
+
+type Usecases struct {
+	Authenticator IAuthenticator
+}
