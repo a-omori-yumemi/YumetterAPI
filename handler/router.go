0fb50ec58ce375908b4da7ecdebf29f0f13abed3
a-omori-yumemi <a_omori@yumemi.co.jp>
2021-10-14 15:14:06 +0900

    impl routing

--- handler/router.go
+++ handler/router.go
@@ -10,7 +10,21 @@ func SetRoute(e *echo.Echo, repos repository.Repositories, services service.Serv
 	g := e.Group("/v1")
 
 	tweetsg := g.Group("/tweets")
-	tweetsg.POST("/", PostTweet(services.TweetService))
 	tweetsg.GET("/:tw_id", GetTweet(services.TweetService))
+	tweetsg.DELETE("/:tw_id", DeleteTweet(services.TweetService))
+	tweetsg.GET("/", GetTweets(services.TweetService))
+	tweetsg.POST("/", PostTweet(services.TweetService))
 
+	usersg := g.Group("/users")
+	usersg.GET("/:usr_id", GetUser(repos.UserRepo))
+	usersg.POST("/", RegisterUser(repos.UserRepo))
+	usersg.POST("/login", LoginUser(repos.UserRepo))
+	usersg.GET("/me", GetMe(repos.UserRepo))
+	usersg.DELETE("/me", DeleteMe(repos.UserRepo))
+	usersg.PATCH("/me", PatchMe(repos.UserRepo))
+
+	favoritesg := tweetsg.Group("/:tw_id/favorites")
+	favoritesg.GET("/", GETFavorites(repos.FavRepo))
+	favoritesg.PUT("/:usr_id", PUTFavorite(repos.FavRepo))
+	favoritesg.DELETE("/:usr_id", DELTEFavorite(repos.FavRepo))
 }
