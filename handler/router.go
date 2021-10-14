169f62e123da7aebbb065e5fd9bff5676b256a33
a-omori-yumemi <a_omori@yumemi.co.jp>
2021-10-14 20:02:14 +0900

    add session context on all endpoints

--- handler/router.go
+++ handler/router.go
@@ -8,23 +8,24 @@ import (
 
 func SetRoute(e *echo.Echo, repos repository.Repositories, usecases usecase.Usecases) {
 	g := e.Group("/v1")
+	g.Use(AuthUserMiddleware(usecases.Authenticator))
 
 	tweetsg := g.Group("/tweets")
 	tweetsg.GET("/:tw_id", GetTweet(usecases.TweetService))
-	tweetsg.DELETE("/:tw_id", DeleteTweet(usecases.TweetService), AuthUserMiddleware(usecases.Authenticator))
-	tweetsg.GET("/", GetTweets(usecases.TweetService))
-	tweetsg.POST("/", PostTweet(usecases.TweetService), AuthUserMiddleware(usecases.Authenticator))
+	tweetsg.DELETE("/:tw_id", DeleteTweet(usecases.TweetService))
+	tweetsg.GET("", GetTweets(usecases.TweetService))
+	tweetsg.POST("", PostTweet(usecases.TweetService))
 
 	usersg := g.Group("/users")
 	usersg.GET("/:usr_id", GetUser(repos.UserRepo))
-	usersg.POST("/", RegisterUser(repos.UserRepo))
+	usersg.POST("", RegisterUser(repos.UserRepo))
 	usersg.POST("/login", LoginUser(usecases.Authenticator))
-	usersg.GET("/me", GetMe(repos.UserRepo), AuthUserMiddleware(usecases.Authenticator))
-	usersg.DELETE("/me", DeleteMe(repos.UserRepo), AuthUserMiddleware(usecases.Authenticator))
-	usersg.PATCH("/me", PatchMe(repos.UserRepo), AuthUserMiddleware(usecases.Authenticator))
+	usersg.GET("/me", GetMe(repos.UserRepo))
+	usersg.DELETE("/me", DeleteMe(repos.UserRepo))
+	usersg.PATCH("/me", PatchMe(repos.UserRepo))
 
 	favoritesg := tweetsg.Group("/:tw_id/favorites")
-	favoritesg.GET("/", GETFavorites(repos.FavRepo))
-	favoritesg.PUT("/:usr_id", PUTFavorite(repos.FavRepo), AuthUserMiddleware(usecases.Authenticator))
-	favoritesg.DELETE("/:usr_id", DELETEFavorite(repos.FavRepo), AuthUserMiddleware(usecases.Authenticator))
+	favoritesg.GET("", GETFavorites(repos.FavRepo))
+	favoritesg.PUT("/:usr_id", PUTFavorite(repos.FavRepo))
+	favoritesg.DELETE("/:usr_id", DELETEFavorite(repos.FavRepo))
 }
