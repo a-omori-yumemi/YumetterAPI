69c38cf44f522dc784c0c3051a07d4025b72adf4
a-omori-yumemi <a_omori@yumemi.co.jp>
2021-10-19 11:55:02 +0900

    fix test

--- yumetter-loadtest/src/test/scala/example/BasicSimulation.scala
+++ yumetter-loadtest/src/test/scala/example/BasicSimulation.scala
@@ -51,8 +51,8 @@ object Tweet {
 class BasicItSimulation extends Simulation {
 
   val httpProtocol = http
-    .baseUrl("http://localhost:8000/v1") // Here is the root for all relative URLs
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
+    .baseUrl(sys.env("YUMETTER_API") + "/v1") // Here is the root for all relative URLs
+    .acceptHeader("application/json;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
