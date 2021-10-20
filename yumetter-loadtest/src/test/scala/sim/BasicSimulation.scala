322341de5a4e976f109766bb2a4b0429a79c9fc9
Naoki Ikeguchi <me@s6n.jp>
2021-10-20 16:20:35 +0900

    refactor: Use separated objects per resource

--- yumetter-loadtest/src/test/scala/sim/BasicSimulation.scala
+++ yumetter-loadtest/src/test/scala/sim/BasicSimulation.scala
@@ -1,112 +1,67 @@
 package sim
 
+import api._
 import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
 import scala.concurrent.duration._
 import scala.util.Random
 import scala.math.exp
-import java.util.Calendar
-
-
-object WatchTL {
-  def calcBuzzIndex(twId: Int) = (0.01 + 0.99 * exp(twId % 500 / 50.0 - 10.0))
-  val watchTL = repeat(3, "n")(
-    exec(
-      http("TL").get("/tweets")
-    )
-  )
-
-  val randomBin = Iterator.continually( Map( "randomBin" -> Random.nextInt(2)))
-
-  val watchFavReply = repeat(5)(
-    exec(
-      http("TL").get("/tweets").check(jsonPath("$[*].tweet").ofType[Map[String,Any]].findAll.saveAs("tweets"))
-    )
-    .foreach("${tweets}", "tweet") {
-      exec{session => 
-        val tweet = session("tweet").as[Map[String, Any]]
-        val buzzIndex = calcBuzzIndex(tweet("tw_id").asInstanceOf[Integer])
-        val doFav = Random.nextDouble() < buzzIndex
-        val doReply = Random.nextDouble() < buzzIndex/2
-        val doLookReply = Random.nextDouble() < buzzIndex/1.2
-        session.set("tw_id", tweet("tw_id")).set("doFav", doFav).set("doReply",doReply).set("doLookReply",doLookReply)
-      }
-      .doIf("${doFav}") {
-        exec(
-          http("Fav").put("/tweets/${tw_id}/favorites/${usr_id}")
-      )}
-      .doIf("${doLookReply}") {
-        exec(
-          http("Show Replies").get("/tweets?replied_to=${tw_id}")
-        )
-      }
-      .doIf("${doReply}") {
-        exec(
-          http("Reply").post("/tweets").formParamMap{session =>
-            Map(
-              "body" -> "reply",
-              "replied_to" -> session("tw_id").as[Int]
-            )
-          }
-        )
-      }.pause(0.2, 2)
-    } 
-  )
-}
-
-object RegisterAndLogin {
-  val reg = exec{session => 
-    val now = Calendar.getInstance()
-    val userName = "U" + now.getTimeInMillis().toString() +Thread.currentThread().getId().toString()
-    println(userName)
-    val password = "pass" + now.getTimeInMillis().toString()
-    session.set("user_name", userName).set("password", password)
-  }.exec(
-    http("Reg").post("/users").formParamMap(Map(
-        "name" -> "${user_name}",
-        "password" -> "${password}"
-    ))
-  ).exec(
-    http("Login").post("/users/login").formParamMap(Map(
-        "name" -> "${user_name}",
-        "password" -> "${password}"
-    ))
-  ).exec(
-    getCookieValue(CookieKey("SESSION").withSecure(true).saveAs("TOO_SECURE_SESSION"))
-  ).exec(
-    addCookie(Cookie("SESSION", "${TOO_SECURE_SESSION}"))
-  ).exec(
-    http("CheckMe").get("/users/me").check(jsonPath("$.usr_id").ofType[Int].saveAs("usr_id"))
-  )
-}
-
-object Tweet {
-  val tweet = exec(
-    http("Tweet").post("/tweets").formParamMap(session=> Map(
-      "body" -> ("Hello " + Random.alphanumeric.take(20).mkString)
-    ))
-  )
-}
 
 class BasicSimulation extends Simulation {
+  def calcBuzzIndex(twId: Int): Double = 0.01 + 0.99 * exp(twId % 500 / 50.0 - 10.0)
 
-  val httpProtocol = http
+  val httpProtocol: HttpProtocolBuilder = http
     .baseUrl(sys.env("YUMETTER_API") + "/v1") // Here is the root for all relative URLs
     .acceptHeader("application/json;q=0.9,*/*;q=0.8") // Here are the common headers
     .acceptEncodingHeader("gzip, deflate")
     .acceptLanguageHeader("en-US,en;q=0.5")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  val scn = scenario("Basic scenario") // A scenario is a chain of requests and pauses
-    .exec(RegisterAndLogin.reg)
+  Random.setSeed(System.currentTimeMillis())
+
+  val feeder: Iterator[Map[String, Any]] = Iterator.continually(
+    Map(
+      "user_name" -> ("U" + System.currentTimeMillis.toString + Thread.currentThread.getId.toString),
+      "password" -> System.currentTimeMillis.toString,
+    )
+  )
+
+  val scn: ScenarioBuilder = scenario("Basic scenario") // A scenario is a chain of requests and pauses
+    .feed(feeder)
+    .exec(
+      Users.post,
+      Users.Login.post,
+      Users.Me.get,
+    )
     .repeat(100)(
-      exec(Tweet.tweet)
-      .exec(WatchTL.watchFavReply)
+      exec(
+        Tweets.post,
+        repeat(5)(
+          Tweets.get.foreach("${tweets}", "tweet") {
+            exec{ session =>
+              val tweet = session("tweet").as[Map[String, Any]]
+              val buzzIndex = calcBuzzIndex(tweet("tw_id").asInstanceOf[Integer])
+
+              session
+                .set("tw_id", tweet("tw_id"))
+                .set("buzz_index", buzzIndex)
+            }
+              .doIf(session => Random.nextDouble() < session("buzzIndex").as[Double]) { Tweets.Favourites.put }
+              .doIf(session => Random.nextDouble() < session("buzzIndex").as[Double] / 2) { Tweets.postReply }
+              .doIf(session => Random.nextDouble() < session("buzzIndex").as[Double] / 1.2) { Tweets.postReply }
+          }
+        )
+      )
     )
 
-  setUp(scn.inject(
-    incrementUsersPerSec(4)
-    .times(50)
-    .eachLevelLasting(10 seconds)
-    .startingFrom(1)).protocols(httpProtocol))
+  setUp(
+    scn.inject(
+      incrementUsersPerSec(4)
+        .times(50)
+        .eachLevelLasting(10 seconds)
+        .startingFrom(1)).protocols(httpProtocol),
+  )
 }
