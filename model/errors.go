ce46c0a0e8ed0f4c29c9007aaf629f58c2977f33
a-omori-yumemi <a_omori@yumemi.co.jp>
2021-10-22 11:25:30 +0900

    tiny fix

--- model/errors.go
+++ model/errors.go
@@ -4,4 +4,4 @@ import "fmt"
 
 var ErrDuplicateKey = fmt.Errorf("duplicate key")
 var ErrNotFound = fmt.Errorf("not found")
-var ErrForbidden error = fmt.Errorf("forbidden operation")
+var ErrForbidden = fmt.Errorf("forbidden operation")
