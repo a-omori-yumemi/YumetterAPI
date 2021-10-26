f01ba8121b56a201788b28f3522d3e070c008438
a-omori-yumemi <a_omori@yumemi.co.jp>
2021-10-26 18:44:04 +0900

    impl poc

--- repository/mysql/favorite.go
+++ repository/mysql/favorite.go
@@ -1,6 +1,10 @@
 package repo_mysql
 
 import (
+	"log"
+	"sync"
+	"time"
+
 	"github.com/a-omori-yumemi/YumetterAPI/db"
 	"github.com/a-omori-yumemi/YumetterAPI/model"
 	"github.com/a-omori-yumemi/YumetterAPI/repository"
@@ -9,11 +13,15 @@ import (
 
 type MySQLFavoriteRepository struct {
 	repository.IFavoriteRepository
-	db db.MySQLDB
+	db       db.MySQLDB
+	asyncFav AsyncAddFavorite
 }
 
 func NewMySQLFavoriteRepository(DB db.MySQLDB) *MySQLFavoriteRepository {
-	return &MySQLFavoriteRepository{db: DB}
+	return &MySQLFavoriteRepository{
+		db:       DB,
+		asyncFav: *NewAsyncAddFavorite(DB),
+	}
 }
 
 func (r *MySQLFavoriteRepository) FindFavorite(TwID model.TwIDType, UsrID model.UsrIDType) (ret model.Favorite, err error) {
@@ -26,10 +34,54 @@ func (r *MySQLFavoriteRepository) FindFavorites(TwID model.TwIDType) (favs []mod
 	return favs, db.InterpretMySQLError(err)
 }
 
+type AsyncAddFavorite struct {
+	addedFavorites []model.Favorite
+	favChan        chan model.Favorite
+	db             db.MySQLDB
+	mutex          sync.Mutex
+}
+
+func NewAsyncAddFavorite(db db.MySQLDB) *AsyncAddFavorite {
+	ret := AsyncAddFavorite{
+		addedFavorites: []model.Favorite{},
+		favChan:        make(chan model.Favorite, 1000000),
+		mutex:          sync.Mutex{},
+		db:             db,
+	}
+	go func() {
+		for {
+			fav := <-ret.favChan
+			ret.mutex.Lock()
+			ret.addedFavorites = append(ret.addedFavorites, fav)
+			ret.mutex.Lock()
+		}
+	}()
+	time.AfterFunc(1*time.Second, ret.CollectFavorites)
+	return &ret
+}
+
+func (f *AsyncAddFavorite) CollectFavorites() {
+	f.mutex.Lock()
+	buf := f.addedFavorites
+	f.addedFavorites = []model.Favorite{}
+	f.mutex.Unlock()
+	if len(buf) > 0 {
+		_, err := f.db.DB.NamedExec("INSERT IGNORE INTO Favorite (tw_id, usr_id) VALUES (:tw_id, :usr_id)", buf)
+		if err != nil {
+			log.Print("Failed to insert ")
+		} else {
+			log.Printf("%d favorites inserted", len(buf))
+		}
+	}
+
+	time.AfterFunc(1*time.Second, f.CollectFavorites)
+}
+
 // ignores duplicate key
 func (r *MySQLFavoriteRepository) AddFavorite(fav model.Favorite) (model.Favorite, error) {
-	_, err := r.db.DB.Exec("INSERT IGNORE INTO Favorite (tw_id, usr_id) VALUES (?,?)", fav.TwID, fav.UsrID)
-	return fav, err
+	// _, err := r.db.DB.Exec("INSERT IGNORE INTO Favorite (tw_id, usr_id) VALUES (?,?)", fav.TwID, fav.UsrID)
+	r.asyncFav.favChan <- fav
+	return fav, nil
 }
 
 // ignores not found key
